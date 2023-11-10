939d2d0235fb7f92db6fa5584d28493055c189ef
Sven Wrieden <wrieden@googlemail.com>
2023-11-10 22:52:33 +0100

    Update manjaro.sh

--- manjaro.sh
+++ manjaro.sh
@@ -1,1 +1,13 @@
-pacman -Syu
+sudo pacman -Syu
+sudo pacman -S --noconfirm yay
+
+touch $HOME/.zshrc
+yay -S --noconfirm zsh 
+sudo chsh -s $(which zsh) $USER
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+yay -S --noconfirm ttf-meslo-nerd-font-powerlevel10k
+
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> $USER/.zshrc
