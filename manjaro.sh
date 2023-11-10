83e80f41d57d075af1785682ee29d0d0586651cc
Sven Wrieden <wrieden@googlemail.com>
2023-11-10 23:07:42 +0100

    Update manjaro.sh

--- manjaro.sh
+++ manjaro.sh
@@ -1,11 +1,12 @@
 sudo pacman -Syu
 sudo pacman -S --noconfirm yay
 
-touch $HOME/.zshrc
 yay -S --noconfirm zsh ttf-meslo-nerd-font-powerlevel10k
 sudo chsh -s $(which zsh) $USER
+rm -rf ~/.oh-my-zsh
 
 git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+sed -i 's\ZSH_THEME="robbyrussell"\ZSH_THEME="powerlevel10k/powerlevel10k"\g' ~/.zshrc
+
