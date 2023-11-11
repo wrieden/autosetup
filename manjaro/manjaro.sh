352dde4b92704402813da3dbc047d4b4d26a2c88
wrieden <6313163+wrieden@users.noreply.github.com>
2023-11-11 19:45:00 +0100

    pre update

--- manjaro/manjaro.sh
+++ manjaro/manjaro.sh
@@ -12,8 +12,8 @@ git config --global user.email 6313163+wrieden@users.noreply.github.com
 # zsh + goodies
 yay -S --noconfirm --needed zsh ttf-meslo-nerd-font-powerlevel10k
 sudo chsh -s $(which zsh) $USER
-rm -rf ~/.oh-my-zsh
 
+rm -rf ~/.oh-my-zsh
 git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 curl -fsSL https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro/.zshrc > ~/.zshrc
@@ -24,6 +24,7 @@ yay -S --noconfirm --needed $(curl -fsSL https://raw.githubusercontent.com/wried
 
 # python
 yay -S --noconfirm --needed python python-pip
+rm -rf ~/.python-venv
 python -m venv ~/.python-venv
 source ~/.python-venv/bin/activate
 pip install $(curl -fsSL https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro/pip-packages.txt)
