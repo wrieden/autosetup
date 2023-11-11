d967c1afba6a57f3fd4c13ac9ab72d9d4b50f680
wrieden <6313163+wrieden@users.noreply.github.com>
2023-11-11 19:14:12 +0100

    updated

--- manjaro/update.sh
+++ manjaro/update.sh
@@ -6,8 +6,8 @@ git -C $setupdir pull --rebase
 
 cp -v ~/.zshrc $setupdir/
 cp -v ~/.p10k.zsh $setupdir/
-echo "$(yay -Qqe)\n$(paclog -f in | sed -En 's/.*installed\s+(\S+).*/\1/p' | sed -n '/paclog/,$p')" | sort | uniq -d | sort -u < $setupdir/packages.txt > $setupdir/packages.txt
-pip-chill --no-version | sort -u < $setupdir/pip-packages.txt > $setupdir/pip-packages.txt
+echo "$(yay -Qqe)\n$(paclog -f in | sed -En 's/.*installed\s+(\S+).*/\1/p' | sed -n '/paclog/,$p')" | sort | uniq -d | sort -u < $setupdir/packages.txt
+pip-chill --no-version | sort -u < $setupdir/pip-packages.txt
 
 git -C $setupdir commit -a -m "updated"
 git -C $setupdir push
