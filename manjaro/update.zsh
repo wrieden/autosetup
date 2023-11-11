06718345ef0034820b294d272fec1117c47eb47a
wrieden <6313163+wrieden@users.noreply.github.com>
2023-11-11 19:35:11 +0100

    start

--- manjaro/update.zsh
+++ manjaro/update.zsh
@@ -2,7 +2,6 @@
 
 setupdir=$(dirname $0)
 
-git -C $setupdir commit -a -m "pre update"
 git -C $setupdir pull --rebase
 
 cp -v ~/.zshrc $setupdir/
@@ -10,5 +9,5 @@ cp -v ~/.p10k.zsh $setupdir/
 echo "$(yay -Qqe)\n$(paclog -f in | sed -En 's/.*installed\s+(\S+).*/\1/p' | sed -n '/paclog/,$p')" | sort | uniq -d | sort -u < $setupdir/packages.txt > $setupdir/packages.txt
 pip-chill --no-version | sort -u < $setupdir/pip-packages.txt > $setupdir/pip-packages.txt
 
-git -C $setupdir commit -a -m "updated"
-git -C $setupdir push
+#git -C $setupdir commit -a -m "updated"
+#git -C $setupdir push
