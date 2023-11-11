5008bb8ef9a8257a934a76fe4f0c24a38b9b3cc2
wrieden <6313163+wrieden@users.noreply.github.com>
2023-11-11 13:24:27 +0100

    git config

--- manjaro/manjaro.sh
+++ manjaro/manjaro.sh
@@ -1,11 +1,20 @@
 sudo pacman -Syu
 sudo pacman -S --noconfirm yay
 
+# git
+yay -S --noconfirm git
+git config --global user.name wrieden
+git config --global user.email 6313163+wrieden@users.noreply.github.com
+
+# zsh + goodies
 yay -S --noconfirm zsh ttf-meslo-nerd-font-powerlevel10k
 sudo chsh -s $(which zsh) $USER
 rm -rf ~/.oh-my-zsh
 
 git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-curl -fsSL https://raw.githubusercontent.com/wRieDen/autosetup/main/manjaro/.zshrc > ~/.zshrc
-curl -fsSL https://raw.githubusercontent.com/wRieDen/autosetup/main/manjaro/.p10k.zsh > ~/.p10k.zsh
+curl -fsSL https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro/.zshrc > ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro/.p10k.zsh > ~/.p10k.zsh
+
+
+
