810a411b7891b3c6603033fe5e3d2da5ab6c1f78
wrieden <6313163+wrieden@users.noreply.github.com>
2023-11-11 21:46:58 +0100

    pre update

--- manjaro/manjaro.sh
+++ manjaro/manjaro.sh
@@ -1,3 +1,5 @@
+repourl=https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro
+
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm yay
 
@@ -6,8 +8,9 @@ yay -S --noconfirm --needed paclog
 
 # git
 yay -S --noconfirm --needed git
-git config --global user.name wrieden
-git config --global user.email 6313163+wrieden@users.noreply.github.com
+curl -fsSL $repourl/sshconfig > ~/.ssh/config
+curl -fsSL $repourl/.gitconfig > ~/.gitconfig
+curl -fsSL $repourl/.gitconfig-trinamic > ~/.gitconfig-trinamic
 
 # zsh + goodies
 yay -S --noconfirm --needed zsh ttf-meslo-nerd-font-powerlevel10k
@@ -16,15 +19,15 @@ sudo chsh -s $(which zsh) $USER
 rm -rf ~/.oh-my-zsh
 git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-curl -fsSL https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro/.zshrc > ~/.zshrc
-curl -fsSL https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro/.p10k.zsh > ~/.p10k.zsh
+curl -fsSL $repourl/.zshrc > ~/.zshrc
+curl -fsSL $repourl/.p10k.zsh > ~/.p10k.zsh
 
 # packages
-yay -S --noconfirm --needed $(curl -fsSL https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro/packages.txt)
+yay -S --noconfirm --needed $(curl -fsSL $repourl/packages.txt)
 
 # python
 yay -S --noconfirm --needed python python-pip
 rm -rf ~/.python-venv
 python -m venv ~/.python-venv
 source ~/.python-venv/bin/activate
-pip install $(curl -fsSL https://raw.githubusercontent.com/wrieden/autosetup/main/manjaro/pip-packages.txt)
+pip install $(curl -fsSL $repourl/pip-packages.txt)
--- manjaro/update.zsh
+++ manjaro/update.zsh
@@ -5,6 +5,9 @@ setupdir=$(dirname $0)
 git -C $setupdir commit -a -m "pre update"
 git -C $setupdir pull --rebase
 
+cp -v ~/.ssh/config $setupdir/sshconfig
+cp -v ~/.gitconfig $setupdir/
+cp -v ~/.gitconfig-trinamic $setupdir/
 cp -v ~/.zshrc $setupdir/
 cp -v ~/.p10k.zsh $setupdir/
 echo "$(yay -Qqe)\n$(paclog -f in | sed -En 's/.*installed\s+(\S+).*/\1/p' | sed -n '/paclog/,$p')" | sort | uniq -d | sort -u < $setupdir/packages.txt > $setupdir/packages.txt
