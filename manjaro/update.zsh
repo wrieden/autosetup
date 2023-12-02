cc9c33685bde16473e3fc85dbb83291b00ebd226
wrieden <6313163+wrieden@users.noreply.github.com>
2023-12-03 00:29:01 +0100

    pre update

--- manjaro/update.zsh
+++ manjaro/update.zsh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-[[ $(command -v paclog) ]] || exit 1
-[[ $(command -v pip-chill) ]] || exit 1
 
 setupdir=$(dirname $0)
 
@@ -16,6 +14,7 @@ cp -v ~/.p10k.zsh $setupdir/
 echo """$(echo "$(yay -Qqe)\n$(paclog -f in | sed -En 's/.*installed\s+(\S+).*/\1/p' | sed -n '/paclog/,$p')" | sort | uniq -d | sort -u < $setupdir/packages.txt)""" > $setupdir/packages.txt
 echo "$(pip-chill --no-version | sort -u < $setupdir/pip-packages.txt)" > $setupdir/pip-packages.txt
 echo "$(code --list-extensions | sort -u < $setupdir/code-packages.txt)" > $setupdir/code-packages.txt
+echo "$(pyocd pack show --no-header | sed -En 's/\s*(\S+).*/\1/p' | sort -u < $setupdir/pyocd-packages.txt)" > $setupdir/pyocd-packages.txt
 
 git -C $setupdir commit -a -m "updated"
 git -C $setupdir push
@@ -23,4 +22,5 @@ git -C $setupdir push
 
 yay -S --noconfirm --needed $(cat $setupdir/packages.txt)
 pip install $(cat $setupdir/pip-packages.txt)
+pyocd pack install $(cat $setupdir/pip-packages.txt)
 echo $(cat $setupdir/code-packages.txt) | xargs -rn1 code --install-extension
