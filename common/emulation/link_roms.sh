a77f6bd9bde5eaa9863a203fabea9fb90d6eb990
wrieden <6313163+wrieden@users.noreply.github.com>
2024-02-28 00:45:04 +0100

    sigle rom vault

--- common/emulation/link_roms.sh
+++ common/emulation/link_roms.sh
@@ -1,7 +1,14 @@
+rm -rf roms/combined
+find media/local -type l -printf "\"%p\" " | xargs -rn1 rm -rf 
+
 cd roms/local
 find . -type d | xargs -rn1 -I{} mkdir -p ../combined/{}
+find . -type d | xargs -rn1 -I{} mkdir -p ../../media/local/{}
 cd ../combined
 find $(readlink -f ../local) -type f | sed -En 's/.*\/roms\/local\/(.*)/"\0" "\1"/p' | xargs -rn2 ln -sf
+cd ../../media/local
+find $(readlink -f ../../roms/local) -type f | sed -En 's/.*\/roms\/local\/(.*)/"\0" "\1"/p' | xargs -rn2 ln -sf   
+
 
 cd ~/Emulation/roms
 find $(readlink -f ~/git/autosetup/common/emulation/roms/combined) -type l | sed -En 's/.*\/roms\/combined\/(.*)/"\0" "\1"/p' | xargs -rn2 ln -sf
--- common/emulation/setup.sh
+++ common/emulation/setup.sh
@@ -9,16 +9,20 @@ cd ..
 curl https://www.romvault.com/download/ROMVault_V3.6.4.zip -o romvault.zip
 7z x -oromvault romvault.zip
 cd romvault
-ln -sf ../dat/processed DatRoot
-ln -sf ../roms/local RomRoot
-ln -sf ../roms/unsorted ToSort
-rm romvault.zip
+rm ../romvault.zip *.xml *.Cache
+ln -s ../RomVault3cfg.xml RomVault3cfg.xml
+cp ../RomVault3_2.Cache RomVault3_2.Cache
+cd ..
 
 curl https://www.skraper.net/download/beta/Skraper-1.1.1.7z -o skraper.7z
 7z x -oskraper skraper.7z
-rm skraper.7z
+cd skraper
+rm ../skraper.7z *.json
+ln -s ../skraper.config.mono.json skraper.config.mono.json
+cd ..
 
 mkdir -p roms/combined
 mkdir -p roms/local
 mkdir -p roms/unsorted
+mkdir -p media/local
 mkdir -p bios
