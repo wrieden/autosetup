2484ab51806abc0627ef0551e105fe73b6cd7458
wrieden <6313163+wrieden@users.noreply.github.com>
2024-03-14 11:46:01 +0100

    pacman update

---
+++ common/snippets/pacman_update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "\n\nUpdating python"
+
+[[ $(command -v yay) ]] || { echo "yay not found" && exit 1 }
+
+yay -Syu --noconfirm
+installed_pkg=$(echo "$(yay -Qqe)\n$(paclog -f in | sed -En 's/.*installed\s+(\S+).*/\1/p' | sed -n '/paclog/,$p')" | sort | uniq -d)
+echo "$(echo $installed_pkg | sort -u < $packages/pacman.txt)" > $packages/pacman.txt
+yay -S --noconfirm --needed $(echo $installed_pkg | sort < $packages/pacman.txt | uniq -u)
+
--- manjaro/update.zsh
+++ manjaro/update.zsh
@@ -8,27 +8,7 @@ git stash
 git pull --rebase
 git stash pop
 
+source $snippets/pacman_update.sh
 source $snippets/python_update.sh
 source $snippets/pyocd_update.sh
 source $snippets/code_update.sh
-
-exit
-
-olddir=$(pwd)
-setupdir=$(dirname $0)
-cd $setupdir
-common=$(dirname $(pwd))/common/
-
-
-echo """$(echo "$(yay -Qqe)\n$(paclog -f in | sed -En 's/.*installed\s+(\S+).*/\1/p' | sed -n '/paclog/,$p')" | sort | uniq -d | sort -u < $common/pacman-packages.txt)""" > $common/pacman-packages.txt
-echo "$(pip list --not-required --exclude-editable --format freeze | sed -En 's/(\S+)==.*/\1/p' | sort -u < $common/pip-packages.txt)" > $common/pip-packages.txt
-echo "$(code --list-extensions | sort -u < $common/code-packages.txt)" > $common/code-packages.txt
-echo "$(pyocd pack show --no-header | sed -En 's/\s*(\S+).*/\1/p' | sort -u < $common/pyocd-packages.txt)" > $common/pyocd-packages.txt
-
-
-yay -S --noconfirm --needed $(cat $common/pacman-packages.txt)
-pip install $(cat $common/pip-packages.txt)
-pyocd pack install $(cat $common/pyocd-packages.txt)
-echo $(cat $common/code-packages.txt) | xargs -rn1 code --install-extension
-
-cd $olddir
