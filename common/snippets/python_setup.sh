e1077bc44ad530514bff9ae260e1608c42b39599
wrieden <6313163+wrieden@users.noreply.github.com>
2024-03-14 14:31:41 +0100

    wheel

--- common/snippets/python_setup.sh
+++ common/snippets/python_setup.sh
@@ -4,4 +4,5 @@ yay -S --noconfirm --needed python python-pip
 rm -rf ~/.python-venv
 python -m venv ~/.python-venv
 source ~/.python-venv/bin/activate
+pip install --upgrade --upgrade-strategy eager wheel
 source $snippets/python_update.sh
