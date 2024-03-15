e87c955ceeb4b1cf85c37d796936f63660034d8a
wrieden <6313163+wrieden@users.noreply.github.com>
2024-03-15 11:48:26 +0100

    ttf droid

--- common/snippets/system_setup.sh
+++ common/snippets/system_setup.sh
@@ -10,3 +10,5 @@ sudo rm -rf /etc/udev/rules.d
 sudo ln -sf $services/udev-rules /etc/udev/rules.d
 sudo udevadm control --reload-rules
 sudo udevadm trigger
+
+gsettings set org.gnome.desktop.interface monospace-font-name "MesloLGS NF Regular 12"
